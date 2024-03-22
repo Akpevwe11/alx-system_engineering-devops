6329a4adc87e5da5ad2f5dff34a5193c2fe28be2
praisex64 <essiakpevwepraise@gmail.com>
2024-03-22 11:07:30 -0700

    inital commit

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,6 @@
-# install flask 2.1.0 from pip3
-exec { 'update pip':
-  command => '/usr/bin/pip3 install --upgrade pip',
-  path    => ['/bin', '/usr/bin'],
-}
+# Installs flask, version 2.1.0.
 
-exec { 'install flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
-  path    => ['/bin', '/usr/bin'],
-  require => Exec['update pip'],
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
